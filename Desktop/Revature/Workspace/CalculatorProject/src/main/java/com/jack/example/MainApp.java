db08e84710f06ed242862a576fb0929beb94bdd5
TharjaBestGirl <jach3079@colorado.edu>
2022-04-04 09:22:29 -0600

    please

---
+++ Desktop/Revature/Workspace/CalculatorProject/src/main/java/com/jack/example/MainApp.java
@@ -0,0 +1,10 @@
+package com.jack.example;
+
+public class MainApp {
+public static void main(String[] args) {
+    Calculator c=new Calculator();
+    
+    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+}
+}
+
---
+++ Desktop/Revature/Workspace/CalculatorProject/src/test/java/com/jack/examples/CalculatorTest.java
@@ -0,0 +1,5 @@
+package com.jack.examples;
+
+public class CalculatorTest {
+
+}
