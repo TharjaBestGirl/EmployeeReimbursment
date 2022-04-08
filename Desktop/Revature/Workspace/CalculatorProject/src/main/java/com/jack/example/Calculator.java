e4127f77bdd0117d70b4c47d8b49110b955276bd
TharjaBestGirl <jach3079@colorado.edu>
2022-04-08 04:00:18 -0600

    please

--- Desktop/Revature/Workspace/CalculatorProject/src/main/java/com/jack/example/Calculator.java
+++ Desktop/Revature/Workspace/CalculatorProject/src/main/java/com/jack/example/Calculator.java
@@ -7,4 +7,19 @@ public class Calculator {
 		return i+j;
 	}
 
+	public int subtract(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}	
+	
+	public int multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return i*j;
+	}
+	
+	public int divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return i/j;
+	}
+
 }
--- Desktop/Revature/Workspace/CalculatorProject/src/test/java/com/jack/examples/CalculatorTest.java
+++ Desktop/Revature/Workspace/CalculatorProject/src/test/java/com/jack/examples/CalculatorTest.java
@@ -1,5 +1,52 @@
 package com.jack.examples;
 
+import org.junit.jupiter.api.*;
+
+import com.jack.example.Calculator;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatorTest {
 
+    static Calculator c=null;
+    
+    //@BeforeEach
+    @BeforeAll
+    static void setUpThis() {
+        System.out.println("Creating Object ");
+        c=new Calculator();
+    }
+    
+    @Test
+    @Order(2)
+    void testAdd() {
+        // c=new Calculator();
+        System.out.println("Testing Add...");
+        Assertions.assertEquals(4, c.add(2, 2));
+        
+    }
+    
+    @Test
+    @Order(1)
+    @Disabled
+    void testSub() {
+         //c=new Calculator();
+        System.out.println("Testing Sub...");
+        Assertions.assertEquals(4, c.subtract(2, 2));
+        
+    }
+    
+    @Test
+    //@Test(expected = ArithmeticException.class)  // JUNIT 4
+    public void testDivision() {
+        System.out.println("Testing Division ");
+        //Assertions.assertEquals(10, c.divide(10, 0));
+        
+          Assertions.assertThrows(ArithmeticException.class,() -> { c.divide(10, 0); });
+            
+    }
+    
+    @AfterAll
+    static void cleanUp() {
+        System.out.println("Cleaning Up...");
+    }
 }
